8a346d915974accf9a67484e990b32c5c5cc3acc
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-01-06 21:57:48 -0300

    Add NewError function

--- definitions/error.go
+++ definitions/error.go
@@ -2,8 +2,17 @@ package definitions
 
 // Error is a wrapper that contains a Base error and additional fields
 type Error struct {
-	Code int
-	Status int
+	Code int64
+	Status int64
 	Message string
-	Base error
+	Base *error
+}
+
+func NewError(code int64, status int64, message string, base *error) *Error {
+	return &Error{
+		Code:    code,
+		Status:  status,
+		Message: message,
+		Base:    base,
+	}
 }
