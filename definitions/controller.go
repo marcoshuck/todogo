b493c1416207f741310a4b8185d89e51888ab6c2
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-01-06 23:55:11 -0300

    Change return type of controller interface methods

--- definitions/controller.go
+++ definitions/controller.go
@@ -1,11 +1,13 @@
 package definitions
 
+import "github.com/marcoshuck/todogo/errors"
+
 // Controller represents a generic REST controller
 type Controller interface {
-	Create(entity interface{}) (interface{}, Error)
-	Read(uuid string) (interface{}, Error)
-	ReadAll() ([]interface{}, Error)
+	Create(entity interface{}) (interface{}, *errors.Error)
+	Read(uuid string) (interface{}, *errors.Error)
+	ReadAll() ([]interface{}, *errors.Error)
 	Count() int
-	Update(uuid string, entity interface{}) (interface{}, Error)
-	Delete(uuid string) (interface{}, Error)
+	Update(uuid string, entity interface{}) (interface{}, *errors.Error)
+	Delete(uuid string) (interface{}, *errors.Error)
 }
