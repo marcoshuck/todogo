f025e2f0ca1a40ee88f6d37ec9a3cdd15c5073b9
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-01-06 22:59:51 -0300

    Add error displaying for listening function

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/marcoshuck/todogo/database"
+	"github.com/marcoshuck/todogo/errors"
 	"github.com/marcoshuck/todogo/router"
 	"github.com/marcoshuck/todogo/server"
 )
 
 func main() {
 	fmt.Println("TO-DO API Rest")
+	var err *errors.Error
 
 	fmt.Println("Setting database up.")
 	db, err := database.New()
@@ -23,7 +25,11 @@ func main() {
 
 	fmt.Println("Setting http server up.")
 	srv := server.New(r, "127.0.0.1", "8080")
-	server.Listen(srv)
+	err = server.Listen(srv)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("[Error] Status: %d | Code: %d | Message: %s", err.Status, err.Code, err.Message))
+		fmt.Println(fmt.Sprintf("Stack trace: %v", err.Base))
+	}
 
 	fmt.Println("Listening and serving http server")
 	return
