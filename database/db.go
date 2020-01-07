2bb04291741286046fc89389240ce72073f80584
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-01-06 21:59:33 -0300

    Add error handling on database package

--- database/db.go
+++ database/db.go
@@ -1,23 +1,30 @@
 package database
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	d "github.com/marcoshuck/todogo/definitions"
+)
 
 type DB struct {
 	*gorm.DB
 }
 
-func New() *DB {
+func New() (*DB, *d.Error) {
 	db, err := gorm.Open("sqlite3", "todo.db")
 
 	if err != nil {
-		panic("Failed to connect to database")
+		return nil, d.NewError(0, 501, "Cannot open database", &err)
 	}
 
 	return &DB{
 		DB: db,
-	}
+	}, nil
 }
 
-func Close(db *DB) {
-	db.Close()
+func Close(db *DB) *d.Error {
+	err := db.Close()
+	if err != nil {
+		return d.NewError(0, 501, "Cannot close database", &err)
+	}
+	return nil
 }
