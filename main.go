983cea2100c2367a0ba014f339fc355b1744a27d
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-01-06 21:45:30 -0300

    Add database implementation in main package

--- main.go
+++ main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"fmt"
+	"github.com/marcoshuck/todogo/database"
 	"github.com/marcoshuck/todogo/router"
 	"github.com/marcoshuck/todogo/server"
 )
 
 func main() {
 	fmt.Println("TO-DO API Rest")
+
+	fmt.Println("Setting database up.")
+	db := database.New()
+	defer database.Close(db)
+
+	fmt.Println("Setting router up.")
 	r := router.New()
+
+	fmt.Println("Setting http server up.")
 	srv := server.New(r, "127.0.0.1", "8080")
 	srv.ListenAndServe()
+
+	fmt.Println("Listening and serving http server")
+	return
 }
