631feeac3f511902b91843c05f1d155b0736a2a5
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-01-07 22:34:30 -0300

    Implement FormatErrorMessage

--- main.go
+++ main.go
@@ -3,36 +3,38 @@ package main
 import (
 	"github.com/marcoshuck/todogo/database"
 	"github.com/marcoshuck/todogo/errors"
-	"github.com/marcoshuck/todogo/logger"
+	log "github.com/marcoshuck/todogo/logger"
 	"github.com/marcoshuck/todogo/router"
 	"github.com/marcoshuck/todogo/server"
 )
 
 func main() {
 	var err *errors.Error
-	var log *logger.Logger
-	log = logger.GetInstance()
+	var logger *log.Logger
+	logger = log.GetInstance()
 
-	log.Info("TO-DO API Rest")
+	logger.Info("TO-DO API Rest")
 
-	log.Info("Setting database up.")
+	logger.Info("Setting database up.")
 	db, err := database.New()
 	if err != nil {
-		log.Error(*err)
+		msg := logger.FormatErrorMessage(*err)
+		logger.Error(msg)
 	}
 	defer database.Close(db)
 
-	log.Info("Setting router up.")
+	logger.Info("Setting router up.")
 	r := router.New()
 
-	log.Info("Setting http server up.")
+	logger.Info("Setting http server up.")
 
 	srv := server.New(r, "127.0.0.1", "8080")
 
-	log.Info("Listening TO-DO API Rest on 127.0.0.1:8080")
+	logger.Info("Listening TO-DO API Rest on 127.0.0.1:8080")
 	err = server.Listen(srv)
 	if err != nil {
-		log.Error(*err)
+		msg := logger.FormatErrorMessage(*err)
+		logger.Error(msg)
 	}
 	return
 }
